a762ccb2254d3bb58c979fa056d313f4abd749c6
--global <--global>
2025-10-20 12:58:11 +0300

    venv kaldırıldı

--- .venv311/Lib/site-packages/morecantile/scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-"""morecantile CLI."""
--- .venv311/Lib/site-packages/server_thread/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa: F401
-from server_thread.report import Report
-from server_thread.server import ServerDownError, ServerManager, ServerThread, launch_server
-
-
-__version__ = "0.3.0"
